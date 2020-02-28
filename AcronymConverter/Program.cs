934d33dc98fac8d45fc74961e22f1f17b03f6c78
Tyler <claypooltyler00@gmail.com>
2020-02-28 09:11:17 -0600

    Created a Prompt method to prompt the user

---
+++ AcronymConverter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AcronymConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void Prompt()
+        {
+            System.Console.WriteLine("This is a program that will ask you you for a multi-word term.");
+            System.Console.WriteLine("Ex. Central Proccessing Unit or Away from Keyboard");
+            System.Console.WriteLine("Then it will convert it to it's  Acroymn");
+        }
+    }
+}
