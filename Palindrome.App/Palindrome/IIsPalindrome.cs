b0afe1e57819fac212a8f8b6a5a9bb012c4057a0
Tyler <claypooltyler00@gmail.com>
2020-03-16 12:35:03 -0500

    My coding assessment

---
+++ Palindrome.App/Palindrome/IIsPalindrome.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindrome
+{
+    interface IIsPalindrome
+    {
+        public bool IsPalindrome(string string1, string string2);
+    }
+}
--- Palindrome.App/Palindrome/Program.cs
+++ Palindrome.App/Palindrome/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Palindrome
 {
-    class Program
+    class Program : IIsPalindrome
     {
         static void Main(string[] args)
         {
             string palindrome = "apple";
+            bool theSame = true;
+
             
             _ = new char[palindrome.Length];
             char[] letter = palindrome.ToCharArray();
@@ -16,12 +17,12 @@ namespace Palindrome
             Console.WriteLine();
             string newString = BackwardWord(palindrome, letter);
             Console.WriteLine();
-            bool theSame = IsSame(palindrome, newString);
+            //bool theSame = IsPalindrome(palindrome, newString);
 
             Console.WriteLine();
             Console.WriteLine(theSame);
             Console.WriteLine();
-            string newWord = ChangeWord(newString, theSame);
+            string newWord = ChangeWord(newString, palindrome, theSame);
             Console.WriteLine(newWord);
         }
 
@@ -49,9 +50,18 @@ namespace Palindrome
             return backwardString;
         }
 
-        static bool IsSame(string string1, string string2)
+        static string ChangeWord(string string2, string string1, bool same2)
         {
-            bool same;
+            //int count = 0;
+
+            string combineString = string1 + string2;
+
+            return combineString;
+        }
+
+        public bool IsPalindrome(string string1, string string2)
+        {
+            bool same = true; ;
             if (string1 == string2)
             {
                 same = true;
@@ -64,14 +74,5 @@ namespace Palindrome
             return same;
         }
 
-        static string ChangeWord(string string2, bool same2)
-        {
-            int i = 0;
-
-            while(false)
-            {
-
-            }
-        }
     }
 }
