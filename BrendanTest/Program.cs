da53531e4346bb23a68f4e268fe92e67c630e8e3
Tyler <claypooltyler00@gmail.com>
2020-03-18 08:43:52 -0500

    Brendan's  test

---
+++ BrendanTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BrendanTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make a string called message and set it to 'Hello my name is Brendan'
+
+
+            //Display the the variable called message
+
+
+            //make 3 int variable called int1, int2, int3 and set the first 2 to 5 and 9 
+            //and set the 3rd to the first variable added together
+
+
+
+
+            //Prompt the user to enter a double and make a varible called myDouble and set it to the user input and display it          It must have a decimal place          ex. 0.0
+
+
+
+
+            //Create an array with the names of everyone in the room amd display them
+
+
+
+            //Make a method and pass the variable message to it
+            //Then in the method change mesage to 'Good-bye' adn display
+
+
+            //inside the method above create and 2nd and thrid string
+            //The second string should be set to "See you tommorrow
+            //The thrid string is equal to the message and the 2nd string put to together
+            //Display the thrid string
+
+
+            //Make a method that will return a boolean and pass int1 and int2
+            //the method should determine if int1 and int2 are same, if they are
+            //The boolean is true, if not they are false and display the boolean in the meain method
+            
+        }
+    }
+}
